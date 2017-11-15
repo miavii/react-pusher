dfd85e7cc12be46e0c6826ff35a23224ea3e8a8b
Mia Lahtinen <beachpanda@hotmail.com>
2017-11-15 20:49:17 +0200

    Working chatapp using react, pusher and Auth0

---
+++ react-pusher/src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: 'zicotic.eu.auth0.com',
+    clientId: 'qqXqeb5g5WR9odkSpk2FAmtO89ONS6fC',
+    callbackUrl: 'http://localhost:3000/callback'
+    //Callback URL set in the Auth0 dasbhoard.
+}
